67be42db804e4be5cbb1b2be28635b70de3987cc
yanatori <yanatori333@gmail.com>
2022-10-16 14:14:24 +0900

    fix:LikeRanking

--- hooks/useQueryCoffees.ts
+++ hooks/useQueryCoffees.ts
@@ -3,15 +3,23 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { Coffee } from "@prisma/client";
 
+type TCount = {
+  _count: {
+    likes: number;
+  };
+};
+
+type TCoffees = Coffee & TCount;
+
 export const useQueryCoffees = () => {
   const router = useRouter();
   const getCoffees = async () => {
-    const { data } = await axios.get<Coffee[]>(
+    const { data } = await axios.get<TCoffees[]>(
       `${process.env.NEXT_PUBLIC_API_URL}/coffee`
     );
     return data;
   };
-  return useQuery<Coffee[], Error>({
+  return useQuery<TCoffees[], Error>({
     queryKey: ["coffees"],
     queryFn: getCoffees,
     onError: (err: any) => {
--- pages/likeRanking.tsx
+++ pages/likeRanking.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { css } from "@emotion/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCrown } from "@fortawesome/free-solid-svg-icons";
-import { useQueryLikes } from "../hooks/useQueryLikes";
 import { useQueryCoffees } from "../hooks/useQueryCoffees";
 import Image from "next/image";
 import NoImage from "../public/noimage.png";
@@ -13,36 +12,38 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import likeFeature from "../utils/likeFeature";
 
 const LikeRanking = () => {
-  const { data: likes } = useQueryLikes();
   const { data: coffees } = useQueryCoffees();
+
   const { onClickLike, likeColor, likeCount } = likeFeature();
 
-  const likesCoffeeId = likes?.map((like) => like.coffeeId);
-  const coffeeIdCount: any = {};
-
-  if (likesCoffeeId !== undefined) {
-    for (let i = 0; i < likesCoffeeId.length; i++) {
-      var elm = likesCoffeeId[i];
-      coffeeIdCount[elm] = (coffeeIdCount[elm] || 0) + 1;
-    }
-  }
-
-  const coffeeIdArray: number[] = Object.values(coffeeIdCount);
-  const bestLike = Math.max(...coffeeIdArray);
-
-  const likeCoffeeArray = Object.entries(coffeeIdCount).map(([key, value]) => ({
-    [key]: value,
-  }));
-
-  const bestLikeCoffeeObj = likeCoffeeArray.filter(
-    (coffee) => Object.values(coffee)[0] === bestLike
+  const coffeeLikeBest = coffees?.filter(
+    (coffee) => coffee._count.likes === coffees[0]._count.likes
   );
 
-  const bestLikeCoffee = coffees?.filter(
-    (coffee) => coffee.id === Number(Object.keys(bestLikeCoffeeObj[0]))
+  const bestCoffeeExcept = coffees?.filter(
+    (coffee, index) =>
+      coffee.id !== (coffeeLikeBest !== undefined && coffeeLikeBest[index]?.id)
   );
 
-  console.log(bestLikeCoffee);
+  const secondCoffee = bestCoffeeExcept?.filter(
+    (coffee) => coffee._count.likes === bestCoffeeExcept[0]._count.likes
+  );
+
+  const coffeeLikeBestArray =
+    coffeeLikeBest !== undefined ? coffeeLikeBest : [];
+
+  const secondCoffeeArray = secondCoffee !== undefined ? secondCoffee : [];
+
+  const bestAndSecond = [...coffeeLikeBestArray, ...secondCoffeeArray];
+
+  const bestAndSecondCoffeeExcept = coffees?.filter(
+    (coffee, index) => coffee !== bestAndSecond[index]
+  );
+
+  const thirdCoffee = bestAndSecondCoffeeExcept?.filter(
+    (coffee) =>
+      coffee._count.likes === bestAndSecondCoffeeExcept[0]._count.likes
+  );
 
   return (
     <div css={likeRankingMainBox}>
@@ -51,81 +52,251 @@ const LikeRanking = () => {
           いいねランキング
           <FontAwesomeIcon icon={faCrown} />
         </h2>
-        {bestLikeCoffee?.map((coffee) => (
-          <div key={coffee.id} css={rankingBox}>
-            <span className="rank">1位</span>
-            <div className="rankingBox__box">
-              <div style={{ width: "48%" }}>
-                {coffee.image !== null ? (
-                  <Image
-                    src={coffee.image}
-                    width={300}
-                    height={300}
-                    alt="画像"
-                  />
-                ) : (
-                  <Image
-                    src={NoImage}
-                    width={300}
-                    height={300}
-                    alt="画像なし"
-                  />
-                )}
-              </div>
-              <div style={{ width: "48%" }}>
-                <div css={explanationBox}>
-                  <span className="explanationBox__text">商品名</span>
-                  <h3>{coffee.name}</h3>
-                </div>
-                <div css={explanationBox}>
-                  <span className="explanationBox__text">カテゴリー</span>
-                  <h3>{coffee.category}</h3>
-                </div>
-                <div css={explanationBox}>
-                  <span className="explanationBox__text">値段</span>
-                  <h3>{coffee.price}</h3>
-                </div>
-                <div css={explanationBox}>
-                  <span className="explanationBox__text">場所</span>
-                  <h3>{coffee.place}</h3>
-                </div>
-                <div css={evaluationMainBox}>
-                  <div css={evaluationBox}>
-                    <Tooltip title="苦さ" placement="top">
-                      <FontAwesomeIcon
-                        icon={faFaceFrown}
-                        className="bitterIcon"
+        {coffeeLikeBest !== undefined && (
+          <div>
+            <span className="rank">
+              {coffeeLikeBest?.length <= 1 ? "1位" : "1位タイ"}
+            </span>
+            {coffeeLikeBest?.map((coffee) => (
+              <div key={coffee.id} css={rankingBox}>
+                <div className="rankingBox__box">
+                  <div style={{ width: "48%" }}>
+                    {coffee.image !== null ? (
+                      <Image
+                        src={coffee.image}
+                        width={300}
+                        height={300}
+                        alt="画像"
                       />
-                    </Tooltip>
-                    {coffee.bitter}
-                  </div>
-                  <div css={evaluationBox}>
-                    <Tooltip title="酸味" placement="top">
-                      <FontAwesomeIcon
-                        icon={faFaceGrinTongue}
-                        className="acidityIcon"
+                    ) : (
+                      <Image
+                        src={NoImage}
+                        width={300}
+                        height={300}
+                        alt="画像なし"
                       />
-                    </Tooltip>
-                    {coffee.acidity}
+                    )}
                   </div>
-                  <div css={evaluationBox}>
-                    <FontAwesomeIcon
-                      icon={faHeart}
-                      className="heartIcon"
-                      onClick={() => onClickLike(coffee.id)}
-                      style={
-                        likeColor(coffee.id)
-                          ? { color: "#e73562" }
-                          : { color: "#bcc7d7" }
-                      }
-                    />
-                    {likeCount(coffee.id)?.length}
+                  <div style={{ width: "48%" }}>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">商品名</span>
+                      <h3>{coffee.name}</h3>
+                    </div>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">カテゴリー</span>
+                      <h3>{coffee.category}</h3>
+                    </div>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">値段</span>
+                      <h3>{coffee.price}</h3>
+                    </div>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">場所</span>
+                      <h3>{coffee.place}</h3>
+                    </div>
+                    <div css={evaluationMainBox}>
+                      <div css={evaluationBox}>
+                        <Tooltip title="苦さ" placement="top">
+                          <FontAwesomeIcon
+                            icon={faFaceFrown}
+                            className="bitterIcon"
+                          />
+                        </Tooltip>
+                        {coffee.bitter}
+                      </div>
+                      <div css={evaluationBox}>
+                        <Tooltip title="酸味" placement="top">
+                          <FontAwesomeIcon
+                            icon={faFaceGrinTongue}
+                            className="acidityIcon"
+                          />
+                        </Tooltip>
+                        {coffee.acidity}
+                      </div>
+                      <div css={evaluationBox}>
+                        <FontAwesomeIcon
+                          icon={faHeart}
+                          className="heartIcon"
+                          onClick={() => onClickLike(coffee.id)}
+                          style={
+                            likeColor(coffee.id)
+                              ? { color: "#e73562" }
+                              : { color: "#bcc7d7" }
+                          }
+                        />
+                        {likeCount(coffee.id)?.length}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
+        )}
+        {/* 2位 */}
+        {secondCoffee !== undefined && (
+          <div>
+            <span className="rank">
+              {secondCoffee?.length <= 1 ? "2位" : "2位タイ"}
+            </span>
+            {secondCoffee?.map((coffee) => (
+              <div key={coffee.id} css={rankingBox}>
+                <div className="rankingBox__box">
+                  <div style={{ width: "48%" }}>
+                    {coffee.image !== null ? (
+                      <Image
+                        src={coffee.image}
+                        width={300}
+                        height={300}
+                        alt="画像"
+                      />
+                    ) : (
+                      <Image
+                        src={NoImage}
+                        width={300}
+                        height={300}
+                        alt="画像なし"
+                      />
+                    )}
+                  </div>
+                  <div style={{ width: "48%" }}>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">商品名</span>
+                      <h3>{coffee.name}</h3>
+                    </div>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">カテゴリー</span>
+                      <h3>{coffee.category}</h3>
+                    </div>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">値段</span>
+                      <h3>{coffee.price}</h3>
+                    </div>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">場所</span>
+                      <h3>{coffee.place}</h3>
+                    </div>
+                    <div css={evaluationMainBox}>
+                      <div css={evaluationBox}>
+                        <Tooltip title="苦さ" placement="top">
+                          <FontAwesomeIcon
+                            icon={faFaceFrown}
+                            className="bitterIcon"
+                          />
+                        </Tooltip>
+                        {coffee.bitter}
+                      </div>
+                      <div css={evaluationBox}>
+                        <Tooltip title="酸味" placement="top">
+                          <FontAwesomeIcon
+                            icon={faFaceGrinTongue}
+                            className="acidityIcon"
+                          />
+                        </Tooltip>
+                        {coffee.acidity}
+                      </div>
+                      <div css={evaluationBox}>
+                        <FontAwesomeIcon
+                          icon={faHeart}
+                          className="heartIcon"
+                          onClick={() => onClickLike(coffee.id)}
+                          style={
+                            likeColor(coffee.id)
+                              ? { color: "#e73562" }
+                              : { color: "#bcc7d7" }
+                          }
+                        />
+                        {likeCount(coffee.id)?.length}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {/* 3位 */}
+        {thirdCoffee !== undefined && (
+          <div>
+            <span className="rank">
+              {thirdCoffee?.length <= 1 ? "3位" : "3位タイ"}
+            </span>
+            {thirdCoffee?.map((coffee) => (
+              <div key={coffee.id} css={rankingBox}>
+                <div className="rankingBox__box">
+                  <div style={{ width: "48%" }}>
+                    {coffee.image !== null ? (
+                      <Image
+                        src={coffee.image}
+                        width={300}
+                        height={300}
+                        alt="画像"
+                      />
+                    ) : (
+                      <Image
+                        src={NoImage}
+                        width={300}
+                        height={300}
+                        alt="画像なし"
+                      />
+                    )}
+                  </div>
+                  <div style={{ width: "48%" }}>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">商品名</span>
+                      <h3>{coffee.name}</h3>
+                    </div>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">カテゴリー</span>
+                      <h3>{coffee.category}</h3>
+                    </div>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">値段</span>
+                      <h3>{coffee.price}</h3>
+                    </div>
+                    <div css={explanationBox}>
+                      <span className="explanationBox__text">場所</span>
+                      <h3>{coffee.place}</h3>
+                    </div>
+                    <div css={evaluationMainBox}>
+                      <div css={evaluationBox}>
+                        <Tooltip title="苦さ" placement="top">
+                          <FontAwesomeIcon
+                            icon={faFaceFrown}
+                            className="bitterIcon"
+                          />
+                        </Tooltip>
+                        {coffee.bitter}
+                      </div>
+                      <div css={evaluationBox}>
+                        <Tooltip title="酸味" placement="top">
+                          <FontAwesomeIcon
+                            icon={faFaceGrinTongue}
+                            className="acidityIcon"
+                          />
+                        </Tooltip>
+                        {coffee.acidity}
+                      </div>
+                      <div css={evaluationBox}>
+                        <FontAwesomeIcon
+                          icon={faHeart}
+                          className="heartIcon"
+                          onClick={() => onClickLike(coffee.id)}
+                          style={
+                            likeColor(coffee.id)
+                              ? { color: "#e73562" }
+                              : { color: "#bcc7d7" }
+                          }
+                        />
+                        {likeCount(coffee.id)?.length}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
@@ -136,7 +307,8 @@ export default LikeRanking;
 const likeRankingMainBox = css`
   background-color: #f7f6f5;
   width: 100%;
-  height: 100vh;
+  height: auto;
+  min-height: 100vh;
 `;
 
 const likeRankingBox = css`
@@ -158,19 +330,21 @@ const likeRankingBox = css`
       width: 30px;
     }
   }
-`;
-
-const rankingBox = css`
-  padding: 20px;
-  border: 2px solid #aaa;
-  border-radius: 20px;
-  overflow-wrap: break-word;
 
   .rank {
     font-size: 28px;
     font-weight: bold;
     color: #fcc800;
   }
+`;
+
+const rankingBox = css`
+  margin: 20px 0;
+  padding: 20px;
+  border: 2px solid #aaa;
+  border-radius: 20px;
+  overflow-wrap: break-word;
+  background-color: #fff;
 
   .rankingBox__box {
     margin: 20px 0;
