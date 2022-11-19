55d2fca50dccef35ebaf1c949f952a0577e67015
yanatori <yanatori333@gmail.com>
2022-11-19 22:45:28 +0900

    fix:useQueryCoffees

--- hooks/useQueryCoffees.ts
+++ hooks/useQueryCoffees.ts
@@ -1,8 +1,7 @@
 import { useRouter } from "next/router";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-import { Coffee } from "@prisma/client";
-import { TCoffeeUser } from "../components/common/CoffeeDetail";
+import { TCoffeeUser } from "../types/coffee";
 
 type TCount = {
   _count: {
@@ -10,17 +9,17 @@ type TCount = {
   };
 };
 
-type TCoffees = Coffee & TCount;
+type TCoffees = TCoffeeUser & TCount;
 
 export const useQueryCoffees = () => {
   const router = useRouter();
   const getCoffees = async () => {
-    const { data } = await axios.get<TCoffeeUser[]>(
+    const { data } = await axios.get<TCoffees[]>(
       `${process.env.NEXT_PUBLIC_API_URL}/coffee`
     );
     return data;
   };
-  return useQuery<TCoffeeUser[], Error>({
+  return useQuery<TCoffees[], Error>({
     queryKey: ["coffees"],
     queryFn: getCoffees,
     onError: (err: any) => {
