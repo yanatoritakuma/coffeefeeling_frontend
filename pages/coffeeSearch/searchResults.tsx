1418c66ad00f6ae8842d6ff1c2c73490a9145b82
yanatori <yanatori333@gmail.com>
2023-01-05 21:28:18 +0900

    add:SearchResults/Pagination

--- pages/coffeeSearch/searchResults.tsx
+++ pages/coffeeSearch/searchResults.tsx
@@ -4,11 +4,14 @@ import { useRouter } from "next/router";
 import CoffeeDetail from "../../components/common/CoffeeDetail";
 import { useQueryCoffeeSearch } from "../../hooks/useQueryCoffeeSearch";
 import { CircularProgress } from "@mui/material";
+import { PaginationBox } from "../../components/common/PaginationBox";
 
 const SearchResults = () => {
   const router = useRouter();
 
   const [transmission, setTransmission] = useState(false);
+
+  const [nowPage, setNowPage] = useState(1);
 
   const coffeeSearchReq = {
     name: String(router.query.name),
@@ -18,6 +21,18 @@ const SearchResults = () => {
   };
 
   const { status, data, refetch } = useQueryCoffeeSearch(coffeeSearchReq);
+
+  // 総ページ数
+  const totalNumber = data !== undefined ? Math.ceil(data.length / 10) : 0;
+  // 現在開いているページのコーヒー
+  const openPageData = data?.slice((nowPage - 1) * 10, nowPage * 10);
+
+  // ページ移動したら画面TOPに戻す
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+    });
+  }, [nowPage]);
 
   // いいねした場合APIを叩く
   const refetchSetTime = () => {
@@ -33,9 +48,22 @@ const SearchResults = () => {
     <div css={searchResultsMainBox}>
       <div css={searchResultsBox}>
         <h2>SearchResults</h2>
-        <div css={coffeeDetailBox}>
-          <CoffeeDetail coffees={data} setTransmission={setTransmission} />
-        </div>
+        {data?.length !== 0 ? (
+          <>
+            <div css={coffeeDetailBox}>
+              <CoffeeDetail coffees={openPageData} setTransmission={setTransmission} />
+            </div>
+            <div css={paginationBox}>
+              <PaginationBox count={totalNumber} nowPage={nowPage} setNowPage={setNowPage} />
+            </div>
+          </>
+        ) : (
+          <h3>
+            検索結果が0件です。
+            <br />
+            別の指定で検索してください。
+          </h3>
+        )}
       </div>
       {status === "loading" && (
         <div className="fileter">
@@ -50,6 +78,8 @@ export default SearchResults;
 
 const searchResultsMainBox = css`
   background-color: #ed7600;
+  width: 100%;
+  min-height: 100vh;
 
   .fileter {
     background-color: #333;
@@ -78,9 +108,29 @@ const searchResultsBox = css`
       font-size: 48px;
     }
   }
+
+  h3 {
+    font-size: 26px;
+    color: #fff;
+    text-align: center;
+    line-height: 1.5em;
+
+    @media screen and (max-width: 425px) {
+      font-size: 20px;
+    }
+  }
 `;
 
 const coffeeDetailBox = css`
   margin: 0 auto;
   max-width: 800px;
+`;
+
+const paginationBox = css`
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px;
+  text-align: center;
+  width: fit-content;
 `;
